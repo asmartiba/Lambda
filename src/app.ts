c7b8462f39ff2f7789ddf5134826cf4c5ced1c29
Asmar <asmartiba@hotmail.com>
2023-03-21 18:59:16 +0100

    landing-page update - not complete

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
