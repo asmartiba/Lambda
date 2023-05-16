40661378e3b734b51120f2574102d8467bc4a4e7
Asmar Tiba <s132986@ap.be>
2023-05-16 21:33:28 +0200

    vercel attempt #10

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import path from 'path';
 const axios = require('axios');
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,  'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
