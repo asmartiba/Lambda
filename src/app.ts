a58675cb52f5d76dc542adf8ebc66cbbbe0a389f
Asmar Tiba <asmartiba@hotmail.com>
2023-05-18 20:13:51 +0200

    favourite in database

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { Collection } from 'mongodb';
+
+
+const {MongoClient} = require('mongodb');
+
+const uri: string = "mongodb+srv://asmartiba:1DZx58W3uFct3NC3@lambda.sfb8kon.mongodb.net/"
+const client = new MongoClient(uri, {useUnifiedTopology: true});
+
+const main = async () => {
+  try {
+    await client.connect();
+    let list = await client.db().admin().listDatabases();
+    console.log(list);
+  }
+  catch(e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main();
 
 const axios = require('axios');
 const app = express();
+app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname,  'views'));
 app.set('view engine', 'ejs');
@@ -29,6 +52,45 @@ app.get('/about', (req, res) => {
 app.get('/home', (req, res) => {
   res.render('home');
 });
+
+
+// favourite page 
+
+interface Favourite {
+  quote: {
+    dialog: string;
+    character: string;
+  };
+}
+
+app.post('/favourite', async (req, res) => {
+  try {
+      const { dialog } = req.body;
+
+      if (typeof dialog !== 'string' || dialog === '') {
+          throw new Error('Invalid data');
+      }
+
+      await client.connect();
+      const db = client.db('LotrDB');
+      const collection = db.collection('favourites');
+
+      const quoteData = {
+          dialog: dialog
+      };
+
+      await collection.insertOne(quoteData);
+
+      res.sendStatus(200);
+  } catch (error) {
+      console.error('Error:', error);
+      res.sendStatus(500);
+  } finally {
+      await client.close();
+  }
+});
+
+
 
 app.get('/favourites', (req, res) => {
   res.render('favourites', {});
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,4 @@
 import app from './app';
-const {MongoClient} = require('mongodb');
-
-const uri: string = "mongodb+srv://asmartiba:1DZx58W3uFct3NC3@lambda.sfb8kon.mongodb.net/"
-const client = new MongoClient(uri, {useUnifiedTopology: true});
-
-
-
-const main = async () => {
-  try {
-    await client.connect();
-    let list = await client.db().admin().listDatabases();
-    console.log(list);
-  }
-  catch(e) {
-    console.log(e);
-  } finally {
-    await client.close();
-  }
-}
-
-main();
 
 const PORT = 3000;
 
